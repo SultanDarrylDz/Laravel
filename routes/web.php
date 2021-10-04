f1982832b886c0bbe2a44f5df27abf3b5e86447c
SultanDarrylDz <sultan.darryl227@gmail.com>
2021-10-04 00:36:26 -0700

    Laravel

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return ('Ini Nomor 1');
 });
+
+// Parameter Wajib
+Route::get('/nama/framework/{nama}/tahun-rilis/{tahun}', function ($nama, $tahun) {
+    return ('Nama Framework yang dimasukan adalah: ' . $nama . '<br>Tahun Rilisnya: ' . $tahun);
+});
+
+// Optional Parameter
+Route::get('/nama-saya-adalah/{nama?}', function ($nama = 'Wajib Diisi') {
+    return 'Nama Saya Adalah: ' . $nama;
+});
+
+Route::get('/menghitung-alas-segitiga/alas/{alas?}/tinggi/{tinggi?}', function ($alas = 1, $tinggi = 1) {
+$luas=$alas+$tinggi*0.5;
+    return 'Alas: ' . $alas . '<br>' . 'Tingginya: ' . $tinggi . '<br>' . 'Luasnya Adalah: ' . $luas;
+});
+
